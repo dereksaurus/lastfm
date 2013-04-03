f1630c0ee0bc8ffb92e267db434268e055b9c6bf
Derek Szeto <derek.szeto@gettyimages.com>
2013-04-03 13:38:09 -0600

    Updated rspec tests

--- spec/lastfm/api_spec.rb
+++ spec/lastfm/api_spec.rb
@@ -6,13 +6,15 @@ describe 'Api' do
     @api = Lastfm.connect "9bf71d8d96122b5e1be6c3f8cffbadd1"
   end
 
-  context "with non-empty string" do
-    it "returns a non-empty string" do
-      @api.get_api_function('get_artist_info').should_not be_empty
-    end
+  describe "Api#get_api_function" do
+    context "with non-empty string" do
+      it "returns a non-empty string" do
+        @api.get_api_function('get_artist_info').should_not be_empty
+      end
 
-    it "returns the string formatted as an api function supported by last.fm" do
-      @api.get_api_function('get_artist_info').should include(:subject => 'artist', :method => 'getinfo')
+      it "returns the string formatted as an api function supported by last.fm" do
+        @api.get_api_function('get_artist_info').should include(:subject => 'artist', :method => 'getinfo')
+      end
     end
   end
 
--- spec/lastfm_spec.rb
+++ spec/lastfm_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe Lastfm do
-  it 'should return correct current version string' do
-    Lastfm.version_string == "LastFM API Wrapper Version #{Lastfm::VERSION}"
+
+  describe "self#version" do
+    it 'should return correct current version string' do
+      Lastfm.version_string == "LastFM API Wrapper Version #{Lastfm::VERSION}"
+    end
   end
+
+  describe "self#connect" do
+    it 'returns an API object when an api key is passed in' do
+      api = Lastfm.connect "api_key"
+      api.should be_kind_of Lastfm::Api
+    end
+  end
+
 end
